df6078b651494092a3f55f24aa30e7fc5505b8bf
Paulo Guerra <paulo.guerra.teran@udla.edu.ec>
2026-04-09 15:42:49 -0500

    template prioridad

---
+++ src/Paciente.java
@@ -0,0 +1,21 @@
+public class Paciente {
+    private int prioridad;
+    private String nombre;
+    private String sintomas;
+
+    public Paciente(int prioridad, String nombre, String sintomas) {
+        this.prioridad = prioridad;
+        this.nombre = nombre;
+        this.sintomas = sintomas;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente" +
+                "prioridad: " + prioridad +
+                ", nombre: " + nombre +
+                ", sintomas: " + sintomas;
+    }
+
+
+}
